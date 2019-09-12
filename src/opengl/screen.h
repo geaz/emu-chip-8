76bd92f48f4dce4e50677d35b59509358a08d74c
Gerrit 'Geaz' Gazic <geaz@users.noreply.github.com>
2019-09-12 21:47:20 +0200

    Changed Buffer Data Format

--- src/opengl/screen.h
+++ src/opengl/screen.h
@@ -42,10 +42,10 @@ class Screen {
          glBindVertexArray(VAO);
 
          glBindBuffer(GL_ARRAY_BUFFER, VBO);
-         glBufferData(GL_ARRAY_BUFFER, sizeofVertices, vertices, GL_STATIC_DRAW);
+         glBufferData(GL_ARRAY_BUFFER, sizeofVertices, vertices, GL_DYNAMIC_DRAW);
 
          glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-         glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeofIndices, indices, GL_STATIC_DRAW);
+         glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeofIndices, indices, GL_DYNAMIC_DRAW);
 
          glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
          glEnableVertexAttribArray(0);
